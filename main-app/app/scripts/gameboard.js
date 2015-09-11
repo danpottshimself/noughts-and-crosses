38cf259fe6afabd6f96d176333fb1e5be96b41fb
Daniel Potts <Daniel.Potts@tombola.com>
2015-09-11 15:59:20 +0100

    edited changes:
    gameBoard created.
    clicking functionality works with numbers that appear for each block displaying the player.
    player choose options added.
    CSS added to make the game appear in the middle and to create game board.
    prevented each block from being selected more than once.
    
    Need to change:
    Remove getelementbyid and insert sprite functionality.
    
    next steps:
    add win conditions.

--- main-app/app/scripts/gameboard.js
+++ main-app/app/scripts/gameboard.js
@@ -4,21 +4,21 @@ myApp = angular.module('Tombola.MyModule', []);
 myApp.controller('MyController', function ($scope) {
     var currentPlayer;
     currentPlayer = '1';
-    $scope.stringLine = '000000000';
+    $scope.gameBoard = '000000000';
     $scope.Player1 = 'player1';
     $scope.Player2 = 'player2';
 
     $scope.chooseBlock = function (index){
-        if ($scope.stringLine.charAt(index)!=="0"){
+        if ($scope.gameBoard.charAt(index)!=="0"){
             return;
         }
         if (currentPlayer === '1'){
-            $scope.stringLine= setCharAt($scope.stringLine ,index,'1');
+            $scope.gameBoard= setCharAt($scope.gameBoard ,index,'1');
             document.getElementsByTagName('img')[index].setAttribute("src", "images/Crosses.gif");
             currentPlayer = '2';
         }
         else {
-            $scope.stringLine = setCharAt($scope.stringLine, index, '2');
+            $scope.gameBoard = setCharAt($scope.gameBoard, index, '2');
             document.getElementsByTagName('img')[index].setAttribute("src", "images/noughts.png");
             currentPlayer = '1';
         }
