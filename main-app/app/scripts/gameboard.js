ce7c994c8a3bb1f1c18d93f633e37479b0fd133e
Daniel Potts <Daniel.Potts@tombola.com>
2015-09-14 13:00:05 +0100

    no message

--- main-app/app/scripts/gameboard.js
+++ main-app/app/scripts/gameboard.js
@@ -8,17 +8,20 @@ myApp.controller('MyController', function ($scope) {
     $scope.Player2 = 'player2';
 
     $scope.chooseBlock = function (index){
-        if ($scope.gameBoard.charAt(index)!=="0"){
+        if($scope.gameBoard.charAt(index)=="111"){
+            alert("you win");
+        }
+        else if ($scope.gameBoard.charAt(index)!=="0"){
             return;
         }
+
         if (currentPlayer === '1'){
             $scope.gameBoard= setCharAt($scope.gameBoard ,index,'1');
-            document.getElementsByTagName('img')[index].setAttribute("src", "images/Crosses.gif");
+          //  document.getElementsByTagName('img')[index].setAttribute("src", "images/Crosses.gif");
             currentPlayer = '2';
         }
         else {
             $scope.gameBoard = setCharAt($scope.gameBoard, index, '2');
-            document.getElementsByTagName('img')[index].setAttribute("src", "images/noughts.png");
             currentPlayer = '1';
         }
     };
@@ -29,3 +32,22 @@ myApp.controller('MyController', function ($scope) {
     }
 });
 
+
+/*
+Top horizontal
+0, 1, 2
+Middle horizontal
+3, 4, 5
+Bottom horizontal
+6, 7, 8
+Left vertical
+0, 3, 6
+Middle vertical
+1, 4, 7
+Right vertical
+2, 5, 8
+Top left diagonal
+0, 4, 8
+Top right diagonal
+2, 4, 6
+*/
