78587517deb776fa5c8833050445a9b25660d508
Daniel Potts <Daniel.Potts@tombola.com>
2015-10-13 12:23:27 +0100

    Created test layout

--- .unit-test/proxy-test.js
+++ .unit-test/proxy-test.js
@@ -1,25 +1,25 @@
-//
-//(function () {
-//    'use strict';
-//    describe('Test GameBoardService', function () {
-//        var constants,
-//            theState,
-//            dataHandler;
-//        beforeEach(function(){
-//            module('Services.MyModule');
-//            inject(function($injector, $http, $q,  _DataHandle_){
-//                theState = $http;
-//                constants = $injector.get('Proxy');
-//                dataHandler = _DataHandle_;
-//            });
-//        });
-//        it('Checks if the newGame function updates the playerTypes on a new game', function(){
-//            var thedata = constants.newGame('human','human');
-//            should(constants.thedata).to.equal('newGame', ['human','human']);
-//        });
-//
-//        afterEach( function(){
-//
-//        });
-//    });
-//}());
+
+(function () {
+    'use strict';
+    describe('Test GameBoardService', function () {
+        var constants,
+            theState,
+            dataHandler;
+        beforeEach(function(){
+            module('Services.MyModule');
+            inject(function($injector, $http, $q,  _DataHandle_){
+                theState = $http;
+                constants = $injector.get('Proxy');
+                dataHandler = _DataHandle_;
+            });
+        });
+        it('Checks if the newGame function updates the playerTypes on a new game', function(){
+            var thedata = constants.newGame('human','human');
+            should(thedata).to.equal('newGame', ['human','human']);
+        });
+
+        afterEach( function(){
+
+        });
+    });
+}());
