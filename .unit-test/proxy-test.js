edcb4d0d4620fae075b1b7bc9502f295f498dac2
Daniel Potts <Daniel.Potts@tombola.com>
2015-10-16 10:56:59 +0100

    Added _proxy_ as a lowercase and moved variables to the top of the tests.

--- .unit-test/proxy-test.js
+++ .unit-test/proxy-test.js
@@ -5,38 +5,43 @@
         var httpBackend,
             returnedPromise,
             proxyName;
+
         beforeEach(function(){
             module('Services.MyModule');
-            inject(function( $httpBackend, $q, _Proxy_  ){
+            inject(['$httpBackend','$q','Proxy', function ( $httpBackend, $q, _proxy_ ){
                 httpBackend = $httpBackend;
                 returnedPromise = $q;
-                proxyName = _Proxy_;
-
-            });
+                proxyName = _proxy_;
+            }]);
         });
+
         it('Checks if the newGame function in the proxy service returns the correct values', function(){
-            var theResponse = {'outcome':'Continue','gameboard':'000000000','winner':0};
+            var theResponse = {'outcome':'Continue','gameboard':'000000000','winner':0},
+                returnedPromise = proxyName.newGame("human", "human"),
+                result;
+
             httpBackend.expectPOST("http://eutaveg-01.tombola.emea:35000/api/v1.0/newgame" , {'player1':"human", 'player2':"human"})
                 .respond(theResponse);
-            var returnedPromise = proxyName.newGame("human", "human");
-            var result;
             returnedPromise.then(function(response){
                 result = response;
             });
+
             httpBackend.flush();
             result.should.be.deep.equal(theResponse);
         });
 
         it('Checks if the proxy make turn function returns the correct values', function(){
-            var theResponse = {'outcome':'Continue','gameboard':'100000000','winner':0};
+            var theResponse = {'outcome':'Continue','gameboard':'100000000','winner':0},
+                returnedPromise = proxyName.playerTurn("1", "0"),
+                result;
+
             httpBackend.expectPOST("http://eutaveg-01.tombola.emea:35000/api/v1.0/makemove" , {"playerNumber": "1",
                 "chosenSquare": "0"})
                 .respond(theResponse);
-            var returnedPromise = proxyName.playerTurn("1", "0");
-            var result;
             returnedPromise.then(function(response){
                 result = response;
             });
+
             httpBackend.flush();
             result.should.be.deep.equal(theResponse);
         });
