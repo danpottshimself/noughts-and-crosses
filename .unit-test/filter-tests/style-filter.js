0c6690ca53c61bad5c166da36ac61ada56296594
Daniel Potts <Daniel.Potts@tombola.com>
2015-10-26 14:24:40 +0000

    Added test for animation filter which returns an animation type by default.

--- .unit-test/filter-tests/style-filter.js
+++ .unit-test/filter-tests/style-filter.js
@@ -1,3 +1,21 @@
-/**
- * Created by daniel.potts on 26/10/15.
- */
+(function () {
+    'use strict';
+    describe('Test StyleFilter', function () {
+        var styleFilter;
+        beforeEach(function(){
+            module('Tombola.NoughtsAndCrosses.Style');
+            inject(function($injector){
+                styleFilter = $injector.get('$filter')('StyleFilter');
+            });
+        });
+
+        it('Checks that the filter is returning the correct results.', function(){
+            var testFilter = styleFilter('main');
+            testFilter.should.equal('css/main.css');
+        });
+        it('Checks that the filter is returning the correct results.', function(){
+            var testFilter = styleFilter('newStyle');
+            testFilter.should.equal('css/newStyle.css');
+        });
+    });
+}());
