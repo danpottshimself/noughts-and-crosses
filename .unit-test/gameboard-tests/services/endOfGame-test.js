2d7aed5744750647f988a3942d67989326cc790a
Daniel Potts <Daniel.Potts@tombola.com>
2015-10-27 13:50:52 +0000

    Added test to check if the go function is being called rather than the state changing.

--- .unit-test/gameboard-tests/services/endOfGame-test.js
+++ .unit-test/gameboard-tests/services/endOfGame-test.js
@@ -1,35 +1,42 @@
 (function () {
     'use strict';
     describe('Test endOfGame', function () {
-        var state,
-            timeout,
+        var timeout,
+            sandbox,
+            stateSpy,
             endedgame;
 
         beforeEach(function(){
             module('ui.router');
             module('Controllers.MyModule');
-            module('Services.MyModule', function(){
+            module('Services.MyModule', function($provide){
+                $provide.value('$state', mocks.StateChange);
             });
 
             inject(function($injector){
-                state= $injector.get('$state');
                 timeout = $injector.get('$timeout');
                 endedgame = $injector.get('EndOfGame')
             });
+            sandbox = sinon.sandbox.create();
+            stateSpy = sinon.sandbox.spy(mocks.StateChange, 'go');
         });
 
         it('Checks that the state changes to draw when game is drawn.', function(){
             endedgame.outcome ='Draw';
             endedgame.gameEnded();
             timeout.flush();
-            state.current.url.should.equal('/draw');
+            stateSpy.should.have.been.calledOnce;
         });
 
         it('Checks that the state changes to draw when game is won.', function(){
             endedgame.outcome ='Win';
             endedgame.gameEnded();
             timeout.flush();
-            state.current.url.should.equal('/winner');
+            stateSpy.should.have.been.calledOnce;
         });
+
+        afterEach(function (){
+            stateSpy.restore();
+        })
     });
 }());
